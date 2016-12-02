8e1acf3f5c07fcb173545151b98a6f8c1eea1800
Tony Golding <tpg@blueyonder.co.uk>
2016-12-02 21:08:36 +0000

    Added market id spec unit test for UK

--- src/main/java/com/tpg/pfm/domain/specs/MarketIdSpecification.java
+++ src/main/java/com/tpg/pfm/domain/specs/MarketIdSpecification.java
@@ -1,15 +1,11 @@
 package com.tpg.pfm.domain.specs;
 
 import com.google.common.base.Optional;
-import com.tpg.pfm.domain.Market;
-import com.tpg.pfm.domain.Markets;
 
 import static com.google.common.base.Optional.of;
 
 public class MarketIdSpecification implements Specification<String> {
     private static final int MARKET_ID_INDEX = 2;
-
-    private final Markets markets = new Markets();
 
     public boolean isSatisfiedBy(String value) {
         String[] tokens = value.split("\\.");
